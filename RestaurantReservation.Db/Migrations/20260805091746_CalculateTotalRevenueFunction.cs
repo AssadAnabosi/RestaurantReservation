8e56fccc56aff271113039bee6810734faa288b7
Assad Anabosi <assad.anabosi@foothillsolutions.com>
2026-08-05 12:23:04 +0300

    feat:Calcualte Total Revenue Func

---
+++ RestaurantReservation.Db/Migrations/20260805091746_CalculateTotalRevenueFunction.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class CalculateTotalRevenueFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var function = """
+                           CREATE OR ALTER FUNCTION dbo.fn_calculate_total_revenue_by_restaurant
+                           (
+                               @restaurant_id int
+                           )
+                           RETURNS decimal(18,2)
+                           AS
+                           BEGIN
+                               DECLARE @total_revenue decimal(18,2);
+
+                               SELECT @total_revenue = COALESCE(SUM(o.total_amount), 0)
+                               FROM orders AS o
+                               INNER JOIN reservations AS r ON r.reservation_id = o.reservation_id
+                               WHERE r.restaurant_id = @restaurant_id;
+
+                               RETURN COALESCE(@total_revenue, 0);
+                           END;
+                           """;
+            
+            migrationBuilder.Sql(function);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS dbo.fn_calculate_total_revenue_by_restaurant;");
+        }
+    }
+}
