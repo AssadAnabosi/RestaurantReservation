204eaa904bea91f819e37464fbdacbca33c0e072
Assad Anabosi <assad.anabosi@foothillsolutions.com>
2026-08-12 14:08:25 +0300

    feat:swagger

--- RestaurantReservation.API/Program.cs
+++ RestaurantReservation.API/Program.cs
@@ -46,12 +46,17 @@ builder.Services
 
 builder.Services.AddAuthorization();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
